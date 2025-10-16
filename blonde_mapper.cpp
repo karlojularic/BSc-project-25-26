982d14457fca0f78a8a18b353fffb53e988b980a
karlojularic <karlo.jularic@fer.hr>
2025-10-16 15:49:22 +0200

    Added optarg, removed test input

--- blonde_mapper.cpp
+++ blonde_mapper.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <unistd.h>
+#include "version.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    int opt;
     
-    cout << "Unesi nesto: " << endl;
-    string input;
-    getline(cin, input);
-    cout << "Unijeli ste: " << input << endl;
+    while ((opt = getopt(argc, argv, ":hv")) != -1) {
+        switch(opt) {
+            case 'h':
+                cout << "Help: This program does something useful." << endl;
+                break;
+            case 'v':
+                cout << "Version: " << PROJECT_VERSION << endl;
+                break;
+        }
+    }
 
     return 0;
     
