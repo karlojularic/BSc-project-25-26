51b5f56f8e1ab26b24f932102150a899653db385
karlojularic <karlo.jularic@fer.hr>
2025-10-16 15:15:14 +0200

    Added CmakeLists and starting code for test

---
+++ blonde_mapper.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Unesi nesto: " << endl;
+    string input;
+    getline(cin, input);
+    cout << "Unijeli ste: " << input << endl;
+
+    return 0;
+    
+}
