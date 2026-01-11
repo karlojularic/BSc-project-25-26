b1e28a45ad5e21533838b5d24cb542f125d1d71e
Katarina Bencun <katarina.bencun@fer.hr>
2026-01-11 18:00:15 +0100

    prilagođen main za poziv mappera

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <getopt.h>
+#include <vector>
 #include "version.h"
 #include "blonde_common.hpp"
 #include "blonde_alignment.hpp"
+#include "blonde_mapper.hpp" 
 
 using namespace std;
 
@@ -11,9 +13,20 @@ void print_help() {
     cout << "Options:\n";
     cout << "  -h, --help       Prikaz pomoći\n";
     cout << "  -v, --version    Prikaz verzije programa\n";
+    cout << "  -k <int>         K-mer size (default: 15)\n";
+    cout << "  -w <int>         Window size (default: 5)\n";
+    cout << "  -f <double>      Filter fraction (default: 0.001)\n";
+    cout << "  -t <int>         Number of threads (default: 1)\n";
+    cout << "  -c               Print CIGAR strings\n";
 }
 
 int main(int argc, char *argv[]) {
+    unsigned int k = 15;
+    unsigned int w = 5;
+    double f = 0.001;
+    int num_threads = 1;
+    bool print_cigar = false;
+
     const struct option long_options[] = {
         {"help",    no_argument, 0, 'h'},
         {"version", no_argument, 0, 'v'},
@@ -22,8 +35,7 @@ int main(int argc, char *argv[]) {
 
     int opt;
     int option_index = 0;
-
-    while((opt = getopt_long(argc, argv, "hv", long_options, &option_index)) != -1) {
+    while((opt = getopt_long(argc, argv, "hvk:w:f:t:c", long_options, &option_index)) != -1) {
         switch(opt) {
             case 'h':
                 print_help();
@@ -31,14 +43,17 @@ int main(int argc, char *argv[]) {
             case 'v':
                 cout << "v" << PROJECT_VERSION << "\n";
                 return 0;
-            default:
-                print_help();
-                return 1;
+            case 'k': k = stoi(optarg); break;
+            case 'w': w = stoi(optarg); break;
+            case 'f': f = stod(optarg); break;
+            case 't': num_threads = stoi(optarg); break;
+            case 'c': print_cigar = true; break;
+            default: print_help(); return 1;
         }
     }
 
     if (optind + 2 != argc) {
-        cerr << "Error: two arguments are expected.\n";
+        cerr << "Error: reference and fragments files are expected.\n";
         print_help();
         return 1;
     }
